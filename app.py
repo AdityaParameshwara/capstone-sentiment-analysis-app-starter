20c3d6abb4129dddfb63ede0149b2a0027eeced2
AdityaParameshwara <adityapbelli.1992@gmail.com>
2026-07-24 00:12:19 +0000

    Integrated VADER analysis and displaying raw results

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 app = Flask(__name__)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # TODO: Write the code that calls the sentiment analysis functions here.
-    # hint: use request.method == "POST"
-    return render_template('form.html')
+    sentiment = None
+
+    if request.method == "POST":
+        analyzer = SentimentIntensityAnalyzer()
+        text = request.form.get("user_text")
+        sentiment = analyzer.polarity_scores(text)
+
+    return render_template('form.html', sentiment=sentiment)
+
 if __name__ == "__main__":
     app.run()
